d54f853f73c41fcf16cc1a32e91f347fa4d3adce
domo <rjonmarco@gmail.com>
2023-11-15 02:04:44 +0800

    create-server.sh

--- create-server.sh
+++ create-server.sh
@@ -1,6 +1,11 @@
+echo "installing git podman python 3 pip"
 yum install -y git podman python3 pip
-pip3 install -y podman-compose
+echo "installing git podman-compose"
+pip3 install podman-compose
+echo "pulling project"
 git pull https://github.com/jmvrgit/Thesis-Project.git
 cd Thesis-Project
+echo "Rootless mode for port 80"
 sysctl net.ipv4.ip_unprivileged_port_start=80 #open port for rootless
+echo "Bringing images up"
 podman-compose up
