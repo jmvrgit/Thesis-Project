3292e7eba066ff90b11f5bdcba6ed73c4d26886a
domo <rjonmarco@gmail.com>
2023-11-21 01:15:37 +0800

    aws

--- create-server.sh
+++ create-server.sh
@@ -7,12 +7,27 @@ cd Thesis-Project; podman-compose up
 
 #Docker
 yum install -y git docker
-sudo systemctl enable docker
-sudo systemctl start docker
-sudo usermod -aG docker $USER
+systemctl enable docker
+systemctl start docker
+usermod -aG docker ec2-user
 newgrp docker
 sudo curl -SL https://github.com/docker/compose/releases/download/v2.23.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 sudo chmod 755 /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+git clone https://github.com/jmvrgit/Thesis-Project.git
+cd Thesis-Project; docker-compose up
+
+#AWS
+#!/bin/bash
+yum install -y git docker
+systemctl enable docker
+systemctl start docker
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.23.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod 755 /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+#! once installed
+usermod -aG docker ec2-user
+newgrp docker
 git clone https://github.com/jmvrgit/Thesis-Project.git
 cd Thesis-Project; docker-compose up
